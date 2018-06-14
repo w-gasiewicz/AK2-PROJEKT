3bb63605f9ed1bbe3797416698d9540d52dbf2a4
Wojciech Gąsiewicz <gasiewicz.wojciech@gmail.com>
2018-06-14 11:13:32 +0200

    Add files via upload

--- pow.s
+++ pow.s
@@ -16,15 +16,25 @@ tekst2: .ascii "Wprowadz potege:\n"
 tekst2_len = .-tekst2
 tekstpotegowanie: .ascii "Zwykłe potegowanie przez mnozenie: \n"
 tekstpot_len= .-tekstpotegowanie
+tekstpotegowanies: .ascii "Program wykonuje potegowanie roznymi metodami: \n"
+tekstpots_len= .-tekstpotegowanies
 tekstszybkiepotegowanie: .ascii "Szybkie potegowanie: \n"
 tekstsz_len = .-tekstszybkiepotegowanie
+tekstpotbin: .ascii "Potegowanie binarne: \n"
+tekstbin_len= .-tekstpotbin
+tekstczas: .ascii "Operacja zajela(cykli procesora): "
+tekstczas_len= .-tekstczas
+enter: .ascii " \n"
+enter_len= .-enter
 
 .bss
 .comm textin1, 512
 .comm textout, 512
 .comm textoutfast, 512
+.comm textoutbin, 512
 .comm textin2, 512
 .comm test, 512
+.comm timeout, 512
 
 .text
 .globl _start
@@ -32,8 +42,8 @@ _start:
 
 mov $SYSWRITE, %rax
 mov $STDOUT, %rdi
-mov $tekstpotegowanie, %rsi
-mov $tekstpot_len, %rdx 
+mov $tekstpotegowanies, %rsi
+mov $tekstpots_len, %rdx 
 syscall
 
 mov $SYSWRITE, %rax
@@ -68,6 +78,9 @@ sub $2, %rax
 mov %rax, %r15
 mov %rax, %rcx  # string reprezentujacy potege
 
+#.globl pow1
+#.type pow1, @function
+#pow1:
 #sprawdzenie czy podstawa jest liczba
 check_if_number1:
 mov textin1(, %rbx, 1), %al
@@ -142,7 +155,12 @@ xor %rax, %rax
 cmp $0, %rcx
 jge decode_number2
 
-#######################
+xor %rax, %rax
+	cpuid
+	rdtsc
+push %rax
+
+####################### algorytm potegowania przez mnozenie
 mov %r8, %rax
 xor %r9,%r9
 mov %r8, %r9
@@ -150,6 +168,9 @@ xor %r14,%r14
 mov %r11,%r14
 dec %r11
 dec %r11
+
+cmp $1, %r14
+je power_length
 
 #petla potegowania
 power_loop:
@@ -184,6 +205,7 @@ dec %r12
 cmp $0, %r12
 jg encode_number
 
+
 #zwykle potegowanie
 mov $SYSWRITE, %rax
 mov $STDOUT, %rdi
@@ -198,6 +220,36 @@ mov $textout, %rsi
 mov $BUFLEN, %rdx
 syscall
 
+#tekst czas
+mov $SYSWRITE, %rax
+mov $STDOUT, %rdi
+mov $tekstczas, %rsi
+mov $tekstczas_len, %rdx
+syscall
+
+#koniec pomiaru czasu
+xor %rax, %rax
+	cpuid
+	rdtsc
+pop %rdi
+
+sub %rdi, %rax #iloc cykli procesora
+mov %rax, timeout
+
+mov $SYSWRITE, %rax
+mov $STDOUT, %rdi
+mov $timeout, %rsi
+mov $BUFLEN, %rdx
+syscall
+#przejscie do nowej linii
+mov $SYSWRITE, %rax
+mov $STDOUT, %rdi
+mov $enter, %rsi
+mov $enter_len, %rdx
+syscall
+
+add $8, %rsp
+
 #szybkie potegowanie:
 mov $SYSWRITE, %rax
 mov $STDOUT, %rdi
@@ -205,11 +257,21 @@ mov $tekstszybkiepotegowanie, %rsi
 mov $tekstsz_len, %rdx
 syscall
 
-#######################
-mov %r14,%r11
+#start pomiaru czasu
+xor %rax, %rax
+	cpuid
+	rdtsc
+push %rax
 
-mov %r9,%r8
+####################### algorytm szybkiego potegowania
+mov %r14,%r11 #potega
+push %r14
+mov %r9,%r8 #podstawa
 xor %r15, %r15 
+mov %r8, %rax
+
+#cmp $1, %r11
+#je konwersja
 
 #spr czy parzysta
 shr %r14
@@ -257,7 +319,6 @@ dec %r11
 mul %r8
 cmp $0, %r11
 jg fast_power_loop2
-#mov %rax,%r13
 mul %rax
 
 konwersja:
@@ -296,8 +357,85 @@ mov $textoutfast, %rsi
 mov $BUFLEN, %rdx
 syscall
 
+
+#poegowanie binarne
+mov $SYSWRITE, %rax
+mov $STDOUT, %rdi
+mov $tekstpotbin, %rsi
+mov $tekstbin_len, %rdx
+syscall
+
+############################ algorytm potegowania binarnego
+pop %r11 #potega
+
+mov %r9,%r8 #podstawa
+mov $8, %r15 #licznik bitow
+xor %r9, %r9
+mov %r11, %r14#potega
+mov $128, %r10 #bylo r10
+mov $1, %rbx
+
+check_length:
+mov %r14, %rax
+and %r10, %rax 
+mov %rax, %r9
+mov $1, %rax
+cmp %r10, %r9
+je subb
+here:
+dec %r15
+shr $1, %r10
+cmp $0, %r15
+je konwersja2
+jmp check_length
+
+
+subb:
+mul %r8
+dec %r9
+cmp $0, %r9
+jg subb
+mul %rbx
+mov %rax, %rbx  # w rbx wynik potegowania
+jmp here
+
+
+konwersja2:
+mov $0, %r12 	# dlugosc ciagu do konwersji
+mov $10, %r10
+mov %rbx, %rax
+
+binpower_length:
+div %r10
+xor %rdx, %rdx
+inc %r12
+cmp $0, %rax
+jg binpower_length
+
+mov %rbx, %rax
+inc %r12
+mov $'\n', %dl
+mov %dl, textoutbin(, %r12, 1)
+dec %r12
+xor %rdx, %rdx
+
+encode_number3:
+div %r10
+add $ASCII_ZERO, %dl
+mov %dl, textoutbin(, %r12, 1)
+xor %rdx, %rdx
+dec %r12
+cmp $0, %r12
+jg encode_number3
+
+#wynik
+mov $SYSWRITE, %rax
+mov $STDOUT, %rdi
+mov $textoutbin, %rsi
+mov $BUFLEN, %rdx
+syscall
 jmp end_label
-xor %rsi, %rsi
+#ret
 
 error_msg:
 mov $SYSWRITE, %rax
